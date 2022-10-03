f350cc152738758718f5558aefbd7b316acf1715
vagabondms <hiseokseok@gmail.com>
2022-10-04 00:31:28 +0900

    chore(.eslintrc withapollo.tsx): eslint rule 추가 및 withApollo 주석 추가

--- src/apollo/withApollo.tsx
+++ src/apollo/withApollo.tsx
@@ -11,19 +11,31 @@ type PageProps = AppProps['pageProps'] & {
   initialCache?: NormalizedCacheObject;
 };
 
-const initializeApolloClient = (initialCache?: NormalizedCacheObject) =>
-  new ApolloClient({
+const initializeApolloClient = (initialCache?: NormalizedCacheObject) => {
+  return new ApolloClient({
     uri: 'http://localhost:3001/api/graphql',
     cache: new InMemoryCache().restore(initialCache ?? {})
   });
+};
 
+/**
+ * next-app 전체에 주입될 client를 만드는 부분.
+ * _app에서 pageProps를 받으면, getApolloServerSideProps에서 넘어온 initialCache를 이용해 최초 cache를 세팅함.
+ * @param ApolloCache getApolloServerSideProps(서버에서)에서 세팅된 cache
+ * @returns ApolloClient ApolloProvider에 인자로 주입될 Client
+ */
 export const createClient = ({ initialCache }: PageProps) => {
   const client = initializeApolloClient(initialCache ?? {});
   return client;
 };
 
+/**
+ * getServerSide에서 일반적인 async 함수를 쓰지 않고 해당 함수를 써야함.
+ * @param callback ApolloClient가 첫번째 인자로, context(일반적인 NextContext)를 두번째 인자로 받는 콜백.
+ * @returns props initialCache를 props에 넣음. 해당 부분을 createClient에서 받아서 사용
+ */
 export const getApolloServerSideProps = (
-  cb: (
+  callback: (
     client: ApolloClient<NormalizedCacheObject>,
     context: GetServerSidePropsContext
   ) => Promise<{
@@ -33,7 +45,7 @@ export const getApolloServerSideProps = (
   const client = initializeApolloClient();
 
   return async (context: GetServerSidePropsContext) => {
-    const { props } = await cb(client, context);
+    const { props } = await callback(client, context);
     const initialCache = client.cache.extract();
     return {
       props: {
--- src/components/common/layout/Default/index.tsx
+++ src/components/common/layout/Default/index.tsx
@@ -4,12 +4,13 @@ import { Footer, Header } from './styled';
 
 interface DefaultProps extends HTMLAttributes<HTMLElement> {}
 
-const Default = ({ children }: DefaultProps) => (
-  <div>
-    <Header>This is Header</Header>
-    {children}
-    <Footer>This is Footer</Footer>
-  </div>
-);
-
+const Default = ({ children }: DefaultProps) => {
+  return (
+    <div>
+      <Header>This is Header</Header>
+      {children}
+      <Footer>This is Footer</Footer>
+    </div>
+  );
+};
 export default Default;
